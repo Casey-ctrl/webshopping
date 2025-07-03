b0aad435912d30a48c724bcd0166051f61df73b8
Casey-ctrl <collinsdidi6@gmail.com>
2025-07-03 19:33:41 +0100

    Counter Program

--- project.js
+++ project.js
@@ -1,13 +1,4 @@
-function dosomething(){
+function doSomething(){
 
-    document.getElementById('Caption').onclick = 'name/age acquired'
-}
-let username = `zandex`;
-let age = `18`
-
-console.log(`This user is ${username}`)
-console.log(`He is ${age} years old`);
-
-function mydisplayer(something){
-    document.getElementById(demo).innerHTML = "something"
+document.getAnimations(slidein).classList.add = "Bounce"
 }
