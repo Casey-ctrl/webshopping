6e6c5d1060779d289c7a67595b0a4eacce7f4983
Casey-ctrl <collinsdidi6@gmail.com>
2025-07-14 13:23:02 +0100

    Js commit

--- webpage/counter.js
+++ webpage/counter.js
@@ -1,8 +1,20 @@
-let time = 14;
+const myCheckbox = document.getElementById('myCheckbox');
+const visaBtn = document.getElementById('visaBtn');
+const masterBtn = document.getElementById('masterBtn');
+const paypalBtn = document.getElementById('paypalBtn');
+const submitBtn = document.getElementById('submitBtn');
+const subResult = document.getElementById('subResult');
+const paymentResult = document.getElementById('paymentResult');
 
-if(time < 12){
-    console.log("Good morning");
+submitBtn.onclick = function(){
+    if(visaBtn.checked){
+        console.log('This User is paying Via VisaCard');
+    }
+    else if(masterBtn.checked){
+        console.log('This User is making Payment Via MasterCard');
+    }
+    else if(paypalBtn.checked){
+        console.log('Payment is being made via payPal');
+    }
 }
-else{
-    console.log("good afternoon");
-}
+
